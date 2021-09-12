cbcf834c323a74de1318398bf1bfc62bae0355a5
chinedu117 <chinedudimonyeka@gmail.com>
2021-09-12 20:59:07 +0100

    Completes start template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div
+      class=" max-w-md mx-auto mt-4 p-4 shadow-md rounded-sm border border-gray-500"
+    >
+      <h3
+        class="w-24 text-4xl px-1 py-1 border-2 border-red-600 text-red-600 text-center my-4"
+      >
+        GTN
+      </h3>
+
+      <div class="font-thin text-xl mb-4">
+        Guess the secret <br />
+        prime number!
+      </div>
+
+      <div class="font-thin text-base  mb-4">
+        The secret number is a prime number under 100. <br />
+        You have to sum up <b>4</b> numbers to reach the secret<br />
+        number. For example, if the number is <b>11</b>, the four <br />clues
+        could be 4, 2, 4, 1.
+      </div>
+
+      <div>
+        <label for="" class="block font-bold">Guess the number</label>
+        <div class="flex ">
+
+          <input type="text" class=" flex-grow border-red-500" />
+
+          <button
+            type="button"
+            class=" ml-3 px-4 bg-blue-600 text-white rounded-sm shadow-md font-bold "
+          >
+            Check
+          </button>
+        </div>
+      </div>
+
+      <div class="mt-4 grid grid-cols-2 gap-2">
+
+          <div class="bg-green-700 text-center text-gray-300 text-3xl font-bold  p-10">?</div>
+          <div class="bg-green-700  text-center text-gray-300 text-3xl font-bold  p-10">?</div>
+          <div class="bg-green-700 text-center text-gray-300 text-3xl font-bold  p-10">?</div>
+          <div class="bg-green-700 text-center text-gray-300 text-3xl font-bold  p-10">?</div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  created () {
+    
+  },
+  name: "Home",
+};
 </script>
